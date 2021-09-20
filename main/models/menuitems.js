9824b62fd63681f3726c7c62e32c717197eca9a1
MistermindX <max.arenstein@gmail.com>
2021-09-20 16:26:49 -0400

    fixed typo

--- main/models/menuitems.js
+++ main/models/menuitems.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class MenuItems extends Model {
     /**
@@ -11,26 +9,29 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      MenuItems.belongsTo(models.Restaurant,{
+      MenuItems.belongsTo(models.Restaurant, {
         foreignKey: 'restaurantId'
       })
     }
-  };
-  MenuItems.init({
-    description: DataTypes.STRING,
-    price: DataTypes.FLOAT,
-    restaurantId:{
-      type: Sequelize.INTEGER,
-      onDelete:'CASCADE',
-      references:{
-      model:'restaurants',
-      key:'id'
-    }
   }
-  }, {
-    sequelize,
-    modelName: 'MenuItems',
-    tableName: 'menu_items'
-  });
-  return MenuItems;
-};
+  MenuItems.init(
+    {
+      description: DataTypes.STRING,
+      price: DataTypes.FLOAT,
+      restaurantId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'restaurants',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: 'MenuItems',
+      tableName: 'menu_items'
+    }
+  )
+  return MenuItems
+}
