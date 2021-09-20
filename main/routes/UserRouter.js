6eab586e31a19a15c4258ad90c29fe72ee308b49
MistermindX <max.arenstein@gmail.com>
2021-09-20 13:03:02 -0400

    created create user and login controllers and routes

--- main/routes/UserRouter.js
+++ main/routes/UserRouter.js
@@ -1,2 +1,7 @@
 const Router = require('express').Router()
 const controller = require('../controllers/UserController')
+
+router.post('/login', controller.LogUserIn)
+router.post('/register', controller.CreateNewUser)
+
+module.exports = Router
