186585542c8602b0b3153c15f38222385de7ef62
MistermindX <max.arenstein@gmail.com>
2021-09-21 12:06:06 -0400

    built menuItem controller and routes

--- main/routes/MenuItemRouter.js
+++ main/routes/MenuItemRouter.js
@@ -2,4 +2,11 @@ const router = require('express').Router()
 const controller = require('../controllers/MenuItemController.js')
 const middleware = require('../middleware')
 
+router.post('/newMenuItem', controller.CreateMenuItem)
+router.get(
+  '/menu/:restaurant_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.FindMenuItemsByRestaurant
+)
 module.exports = router
