7a2c0b1556752956a6c964f9a0defbbf2cb87d76
MistermindX <max.arenstein@gmail.com>
2021-09-21 10:01:26 -0400

    wrote out restaurant controllers and routes

--- main/routes/RestaurantRouter.js
+++ main/routes/RestaurantRouter.js
@@ -2,4 +2,9 @@ const router = require('express').Router()
 const controller = require('../controllers/RestaurantController')
 const middleware = require('../middleware')
 
+router.get('/allrestaurants', controller.GetAllResturants)
+router.post('/newrestaurant', controller.CreateRestaurant)
+router.get('/search/state/:state', controller.FindRestaurantByState)
+router.get('/search/food/:food', controller.FindRestaurantByFood)
+router.get('/search/name/:restaurant_name', controller.FindRestaurantByName)
 module.exports = router
