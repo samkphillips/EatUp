a910bd190d1416501539cddf9aa5cd1e96a58f26
Sam <sam.k.phillips@gmail.com>
2021-09-22 16:40:57 -0400

    Scratched out the past orders page

--- main/client/src/pages/MyOrders.js
+++ main/client/src/pages/MyOrders.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GetOrdersByUser } from '../services/ProtectedServices'
 
-export default function MyOrders() {
+export default function MyOrders(props) {
+  const [orders, setOrders] = useState([])
+
+  const getOrders = async () => {
+    const res = await GetOrdersByUser(props.user.id)
+    //setOrders(res)
+
+    let parsedOrderList = []
+
+    // parsedOrderList = [
+    //   {orderItems:[{item:"food1", qty: 3},{},{}], restaurant: id, date:createdDate},
+    //   {}
+    // ]
+
+    res.forEach((order) => {
+      let parsedOrder = {
+        orderItems: [],
+        restaurantId: order.restaurantId,
+        orderDate: order.createdAt
+      }
+
+      order.orderItems.forEach((taco) => {
+        let matchFound = false
+
+        parsedOrder.orderItems.forEach((o) => {
+          if (taco === o.id && !matchFound) {
+            matchFound = true
+            o.qty++
+          }
+        })
+
+        if (!matchFound) {
+          //we need an axios call to get menuItem details right here.
+          parsedOrder.orderItems.push({ id: taco, qty: 1 })
+        }
+      })
+
+      parsedOrderList.push(parsedOrder)
+    })
+
+    await setOrders(parsedOrderList)
+    console.log(orders)
+  }
+
+  useEffect(() => {
+    getOrders()
+  }, [])
+
   return (
     <div>
       <h1>Past Orders</h1>
+      {orders.length > 0 ? (
+        orders.map((item) => (
+          <div>
+            <h3>{item.restaurantId}</h3>
+            {/* <h5>{item.orderItem}</h5> */}
+            {item.orderItems.map((i) => (
+              <h5>{`${i.id} qty ${i.qty}`}</h5>
+            ))}
+          </div>
+        ))
+      ) : (
+        <h3>You do not have any current orders.</h3>
+      )}
     </div>
   )
 }
