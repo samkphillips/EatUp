fd4c0d0f854a58ef5145f8a513779678bf9f0a65
Sam <sam.k.phillips@gmail.com>
2021-09-22 22:20:40 -0400

    Real sign in works

--- main/client/src/pages/SignIn.js
+++ main/client/src/pages/SignIn.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SignInUser } from '../services/Auth'
 
 export default function SignIn(props) {
+  const [signInFormValues, setSignInFormValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  const signInHandleChange = (e) => {
+    setSignInFormValues({
+      ...signInFormValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SignInUser(signInFormValues)
+    setSignInFormValues({ email: '', password: '' })
+    props.setUser(payload)
+    props.toggleAuthenticated(true)
+    props.history.push('/feed')
+  }
+
   const tempSignIn = async (e) => {
     const payload = await SignInUser({
       email: 'example@fakemail.com',
@@ -13,9 +34,42 @@ export default function SignIn(props) {
   }
 
   return (
-    <div>
-      <h1>Sign In or Sign Up</h1>
-      <button onClick={tempSignIn}>Sign In</button>
+    <div className="sign-in-page">
+      <div>
+        <h1>Sign In</h1>
+        <button onClick={tempSignIn}>Fake Sign In</button>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={signInHandleChange}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={signInFormValues.email}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={signInHandleChange}
+              type="password"
+              name="password"
+              value={signInFormValues.password}
+              required
+            />
+          </div>
+          <button
+            disabled={!signInFormValues.email || !signInFormValues.password}
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+      <div>
+        <h1>Sign Up</h1>
+      </div>
     </div>
   )
 }
