aed783d23b0152956f2bfe94fab1b398d0bf55d3
MistermindX <max.arenstein@gmail.com>
2021-09-23 11:57:58 -0400

    added checkout form and updated server.js

--- main/server.js
+++ main/server.js
@@ -2,10 +2,30 @@ const app = require('express')()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+require('dotenv').config()
+const stripe = require('stripe')(`${STRIPE_SECRET_KEY}`)
 
 const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400
+}
+app.post('/create-payment-intent', async (req, res) => {
+  const { items } = req.body
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: 'usd'
+  })
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+})
 
 app.use(cors())
 app.use(morgan('dev'))
