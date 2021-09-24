a39ec618c9c60c91bc3fc1358823b2e287be57a3
Sam <sam.k.phillips@gmail.com>
2021-09-24 10:51:00 -0400

    Deploying

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('express')()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const path = require('path')
 
 const AppRouter = require('./routes/AppRouter')
 
@@ -13,4 +14,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api', AppRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
